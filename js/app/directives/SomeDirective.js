b839e1afc8ea49e00870364a39adb11d105891c6
Todd Motto <todd@toddmotto.com>
2016-03-15 01:20:44 +0000

    chore: added changes

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -9,9 +9,16 @@ function SomeDirective() {
 		controller: function() {
 			this.text = 'Replace this text!';
 		},
+		require: 'someDirective',
 		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+		link: function (scope, elem, attrs, ctrl) {
+			var el = elem[0];
 
+			var span = el.querySelector('span');
+			el.addEventListener('click', function () {
+				span.textContent = 'Thank you for clicking!';
+				scope.$apply();
+			});
 		}
 	}
 }
